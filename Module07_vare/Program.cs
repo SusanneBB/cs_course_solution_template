a5495b350f05c1d8d2963ab7727177bd16f17f45
SusanneBB <sonitosu@stofanet.dk>
2017-11-13 15:07:37 +0100

    Nu med vareliste

--- Module07_vare/Program.cs
+++ Module07_vare/Program.cs
@@ -17,6 +17,19 @@ namespace Module07_vare
             Vare v2 = new Vare(20, "hus forbi");
             Console.WriteLine(v1.Navn + " med moms koster " + v1.PrisMedMoms());
             Console.WriteLine(v2.Navn + " med moms koster " + v2.PrisMedMoms());
+
+            int antal = 3;
+            Vare[] VareListe = new Vare[antal];
+
+            VareListe[0] = new Vare(50, "Gave");
+            VareListe[1] = new Vare(75, "Bøffer");
+            VareListe[2] = new Vare(100, "Rødvin");
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(VareListe[i].Navn + " koster med moms " + VareListe[i].PrisMedMoms());
+            }
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 System.Console.Write("Press any key to continue . . . ");
