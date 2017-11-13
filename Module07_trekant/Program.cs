5ed65eb2a18a059f5fe0acd76bde43f629938319
SusanneBB <sonitosu@stofanet.dk>
2017-11-13 15:22:06 +0100

    Trekanter

---
+++ Module07_trekant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_trekant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trekant t1 = new Trekant(1, 1);
+            Console.WriteLine("Areal burde være 1/2 = " + t1.Areal.ToString("N2"));
+
+            Trekant t2 = new Trekant(2, 2);
+            Console.WriteLine("Areal burde være 2 = " + t2.Areal.ToString("N2"));
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Module07_trekant/Trekant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_trekant
+{
+    class Trekant
+    {
+        public int Grundlinje { get; private set; }
+
+        public int Højde { get; private set; }
+
+        public double Areal
+        {
+        get
+            {
+                return (Højde * Grundlinje) / 2.00;
+            }
+        }
+
+        public Trekant(int højde, int grundlinje)
+        {
+            Højde = højde;
+            Grundlinje = grundlinje;
+        }
+
+    }
+}
