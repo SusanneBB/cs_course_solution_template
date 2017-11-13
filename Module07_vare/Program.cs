15bdee839abc91e560ccf6b82cb658fffb074cd6
SusanneBB <sonitosu@stofanet.dk>
2017-11-13 14:57:21 +0100

    Module07_vare

---
+++ Module07_vare/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_vare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vare v1 = new Vare();
+            v1.Navn = "Rugbrød";
+            v1.Pris = 20;
+            Vare v2 = new Vare(20, "hus forbi");
+            Console.WriteLine(v1.Navn + " med moms koster " + v1.PrisMedMoms());
+            Console.WriteLine(v2.Navn + " med moms koster " + v2.PrisMedMoms());
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Module07_vare/Vare.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_vare
+{
+    class Vare
+    {
+        private string navn;
+
+        public Vare()
+        {
+
+        }
+
+        public Vare(int pris, string navn)
+        {
+            this.Navn = navn;
+            this.Pris = pris;
+
+        }
+        public string Navn
+        {
+            get
+            {
+                Console.WriteLine("Nu læser vi navnet");
+                return navn;
+            }
+            set
+            {
+                Console.WriteLine("Nu er der nogen der angiver navnet til " + value);
+                navn = value; }
+        }
+
+        private double pris;
+
+        public double Pris
+        {
+            get
+            {
+                Console.WriteLine("Nu er der nogen der læser prisen");
+                return pris;
+            }
+            set
+            {
+                Console.WriteLine("Nu er der nogen der sætter prisen til " + value.ToString() + "!!");
+                pris = value;
+            }
+        }
+
+        public double PrisMedMoms()
+        {
+        return this.pris * 1.25;
+        }
+    }
+}
