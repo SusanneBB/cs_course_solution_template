91050b13516b520f684f932de8c0edda50cb389b
SusanneBB <sonitosu@stofanet.dk>
2017-11-13 13:30:47 +0100

    Module06_terning

---
+++ Module06_Terning/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module06_Terning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t1 = new Terning();
+            t1.Skriv();
+            for (int i = 0; i < 10; i++)
+            {
+                t1.Ryst();
+                t1.Skriv();
+            }
+
+            Terning t2 = new Terning(true);
+            t2.Skriv();
+            for (int i = 0; i < 10; i++)
+            {
+                t2.Ryst();
+                t2.Skriv();
+            }
+
+            Terning t3 = new Terning();
+            int antal = 150;
+            var kast = new int[antal]; 
+            for (int i = 0; i < antal; i++)
+            {
+                t3.Ryst();
+                kast[i] = t3.værdi;
+            }
+            Console.WriteLine("Efter " + antal.ToString() + " kast er snittet " + kast.Average().ToString("N2"));
+
+            Terning t4 = new Terning(true);
+            for (int i = 0; i < antal; i++)
+            {
+                t4.Ryst();
+                kast[i] = t4.værdi;
+            }
+            Console.WriteLine("Efter " + antal.ToString() + " kast er snittet med snyd sjovt nok " + kast.Average().ToString("N2"));
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Module06_Terning/Terning.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module06_Terning
+{
+    class Terning
+    {
+        private static System.Random rnd = new System.Random();
+
+        public int værdi;
+
+        private bool snyd;
+
+        public Terning()
+        {
+            værdi = 1;
+            snyd = false;
+        }
+
+        public Terning(bool JegVilSnyde)
+        {
+            værdi = 1;
+            snyd = JegVilSnyde;
+        }
+        public void Skriv()
+        {
+            if (snyd)
+            {
+                Console.WriteLine("Værdi med snyd: " + værdi.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Værdi uden snyd: " + værdi.ToString());
+            }
+        }
+
+        public void Ryst()
+        {
+            if (snyd)
+            {
+                værdi = 6;
+            }
+            else
+            {
+                værdi = rnd.Next(1, 7);
+            }
+        }
+    }
+}
