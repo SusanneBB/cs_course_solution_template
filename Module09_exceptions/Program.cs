59c16d13c6f2f317d48786cc0a1506d14f95def8
SusanneBB <sonitosu@stofanet.dk>
2017-11-14 14:52:32 +0100

    Exception - ikke et tal

---
+++ Module09_exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module09_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Der er sket en fejl: {ex.Message}");
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+        private static void Run()
+        {
+            Console.WriteLine("Indtast 1. tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int res = tal1 + tal2;
+            Console.WriteLine($"Resultat er {res}");
+
+        }
+    }
+}
