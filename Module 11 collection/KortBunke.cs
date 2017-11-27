76d56d54d33f5f2f797fd8042381609cc81327a9
admin <admin@TA-8215-08>
2017-11-27 14:23:33 +0100

    Collection

---
+++ Module 11 collection/KortBunke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_collection
+{
+    class KortBunke
+    {
+        private Stack<Kort> Bunke = new Stack<Kort>();
+
+        public void TilføjKort(Kort k)
+        {
+            Bunke.Push(k);
+        }
+
+        public Kort FjernKort()
+        {
+            return Bunke.Pop();
+        }
+
+        public void VisKort()
+        {
+            foreach (var kort  in Bunke)
+            {
+                Console.WriteLine(kort.ToString()); 
+            }
+        }
+    }
+}
---
+++ Module 11 collection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KortBunke b = new KortBunke();
+            b.TilføjKort(new Kort() {Kulør = "Spar", Værdi = 2 });
+            b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 14 });
+            b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 3 });
+            b.VisKort();
+
+            var k = b.FjernKort();
+            Console.WriteLine("");
+            Console.WriteLine(k);
+            Console.WriteLine("");
+
+            b.VisKort();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
