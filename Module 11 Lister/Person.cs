a7a83bbee77c8abfae499bb47889d8ae5c28efe4
admin <admin@TA-8215-08>
2017-11-27 14:12:50 +0100

    Lister og dictionary

---
+++ Module 11 Lister/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_Lister
+{
+    class Person
+    {
+        public Person(int id, string navn)
+        {
+            Id = id;
+            Navn = navn;
+        }
+        public int Id { get; set; }
+        public string Navn { get; set; }
+    }
+}
---
+++ Module 11 Lister/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_Lister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List< Person> familie = new List<Person>();
+            familie.Add(new Person(1, "Susanne"));
+            familie.Add(new Person(2, "Torben"));
+            familie.Add(new Person(3, "Nina"));
+            familie.Add(new Person(4, "Sofia"));
+            Console.WriteLine("SOM LISTE");
+            foreach (var f in familie)
+            {
+                Console.WriteLine($"Her er familie medlemd {f.Navn} med id {f.Id}");
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("SOM DICTIONARY");
+            Dictionary<int, Person> familie2 = new Dictionary<int, Person>();
+            familie2.Add(5, new Person(55, "Mor"));
+            familie2.Add(6, new Person(66, "Far"));
+            familie2.Add(7, new Person(77, "Søster"));
+            foreach (var f2 in familie2)
+            {
+                Console.WriteLine($"Her familie2 medlem {f2.Value.Navn} med id {f2.Value.Id} og nøgle {f2.Key.ToString()}");
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
