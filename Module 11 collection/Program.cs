ecc82b463452c790b409ff7801bcc6f5b4e304ac
admin <admin@TA-8215-08>
2017-11-27 14:42:18 +0100

    Spillekort

--- Module 11 collection/Program.cs
+++ Module 11 collection/Program.cs
@@ -11,7 +11,7 @@ namespace Module_11_collection
         static void Main(string[] args)
         {
             KortBunke b = new KortBunke();
-            b.TilføjKort(new Kort() {Kulør = "Spar", Værdi = 2 });
+            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi = 2 });
             b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 14 });
             b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 3 });
             b.VisKort();
@@ -22,6 +22,24 @@ namespace Module_11_collection
             Console.WriteLine("");
 
             b.VisKort();
+
+            Console.WriteLine("");
+            KortBunke sk = new KortBunke();
+            List<string> KortFarver = new List<string>();
+            KortFarver.Add("Hjerter");
+            KortFarver.Add("Spar");
+            KortFarver.Add("Klør");
+            KortFarver.Add("Ruder");
+
+            foreach (var kf in KortFarver)
+            {
+                for (int i = 13; i > 0; i--)
+                {
+                sk.TilføjKort(new Kort() { Kulør = kf, Værdi = i });
+                }
+            }
+            Console.WriteLine("Spillekort");
+            sk.VisKort();
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
