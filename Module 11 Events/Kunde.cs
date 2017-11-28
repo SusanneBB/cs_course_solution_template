0ea6b73de25addfd23d9cf50dc3c323c701ae6c5
admin <admin@TA-8215-08>
2017-11-28 13:25:20 +0100

    Event ved overtræk

---
+++ Module 11 Events/Kunde.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_Events
+{
+    class Kunde
+    {
+        public string Navn { get; set; }
+        public int Saldo { get; set; }
+        public int KreditMax  {get; set; }
+
+        public event EventHandler KreditOverskredet;
+
+        public void Køb(int værdi)
+        {
+            Console.WriteLine("Kunde {0} køber for {1}", Navn, værdi);
+            this.Saldo -= værdi;
+            if (Saldo < KreditMax)
+            {
+                KreditOverskredet(this, new EventArgs());
+            }
+        }
+
+        public void Indsæt(int værdi)
+        {
+            this.Saldo += værdi;
+        }
+    }
+}
---
+++ Module 11 Events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_11_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kunde k = new Kunde() { Navn = "Susanne", KreditMax = -500 };
+            k.KreditOverskredet += (s, e) => { Console.WriteLine($"Krediten er overskredet for {k.Navn}"); };
+            k.Køb(100);
+            Console.WriteLine("Saldo er nu " + k.Saldo.ToString());
+            k.Køb(401);
+            Console.WriteLine("Saldo er nu " + k.Saldo.ToString());
+            k.Indsæt(100);
+            Console.WriteLine("Saldo er nu " + k.Saldo.ToString());
+            k.Køb(95);
+            Console.WriteLine("Saldo er nu " + k.Saldo.ToString());
+            k.Køb(10);
+            Console.WriteLine("Saldo er nu " + k.Saldo.ToString());
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
